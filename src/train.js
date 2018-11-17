d0b88da0ec51bb03af4e62befa8b4bc4a098f11c
Eddie Jaoude <eddie@jaoudestudios.com>
2018-11-17 18:03:05 +0100

    Example train file added

---
+++ src/train.js
@@ -0,0 +1,11 @@
+// 1. get list of files
+
+// 2. extract category from file name
+// example filename df123_high.jpg
+
+const filename = 'df123_low.jpg';
+
+// const category = 'low';
+console.log(category);
+
+// 3. send file + category to AI
